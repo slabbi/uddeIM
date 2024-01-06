57a0afb73a0f85d28472f2839d3ca0c3d9a214be
joomod <149375504+joomod@users.noreply.github.com>
2024-01-06 20:10:59 +0100

    Update admin.uddeim.php
    clear opcache to reload the config file

--- _uddeIM5_for_Joomla5_UNSUPPORTED/com_uddeim_j50/admin/admin.uddeim.php
+++ _uddeIM5_for_Joomla5_UNSUPPORTED/com_uddeim_j50/admin/admin.uddeim.php
@@ -457,7 +457,7 @@ function uddeIMsaveSettings($option, $task, $pathtoadmin, $config) {
 		$redirecturl = uddeIMredirectIndex()."?option=com_uddeim&task=archivetotrash";
 		uddeIMmosRedirect($redirecturl, $mosmsg);
 	}
-	
+	opcache_reset();
 	$mosmsg=_UDDEADM_SETTINGSSAVED;
 	$redirecturl = uddeIMredirectIndex()."?option=com_uddeim&task=settings";
 	uddeIMmosRedirect($redirecturl, $mosmsg);
