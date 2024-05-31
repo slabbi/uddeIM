bf2f47ee693544e1a7901f3ff2279099ab0548e6
joomod <149375504+joomod@users.noreply.github.com>
2024-05-31 15:32:03 +0200

    Add files via upload
    overview new language defines, needed if using other (old) language scripts

---
+++ com_uddeim_j50/admin/language.utf8/new_defines_v5+.php
@@ -0,0 +1,37 @@
+<?php
+
+// New: 5.4
+DEFINE ('_UDDEIM_CHARS', 'chars');
+DEFINE ('_UDDEADM_CAPTCHA_NUM', 'UddeIM NUM /robot field');
+// New: 5.3
+DEFINE ('_UDDEADM_REVIEWBACKUP', 'Important: during installation a new <b>Config File</b> (config.class.php) was created - the old file was saved as config.class_bak.php. It may be renamed to replace the new one, but it is <b>recommanded</b> to edit the configuration manually, to see the changes');
+DEFINE ('_UDDEADM_CFGFILE_CONVERTING_18', '...set default values for Autocompleter and saveconfigdb');
+DEFINE ('_UDDEADM_CFGFILE_CONVERTING_0', 'update expects min. config v2.6 or higher');
+DEFINE ('_UDDEADM_SAVECONFIGDB_HEAD', 'always save Configuration to db');
+DEFINE ('_UDDEADM_SAVECONFIGDB_EXP', 'always backup the configuration on each saving to the db');
+
+// New: 5.0
+DEFINE ('_UDDEIM_MESSAGE_REPLY_INFO', ' (with Infomail)');
+DEFINE ('_UDDEIM_MESSAGEINFO_SENT', 'Message and Infomail sent to ');
+DEFINE ('_UDDEIM_MESSAGEINFO_ERROR', 'Message sent, but Infomail failed');
+DEFINE ('_UDDEADM_CONFIGNOTE', 'NOTE: because of Cache settings changes may not be visible after save and reload');
+DEFINE ('_UDDEADM_KUNENA6', 'Kunena 6.0+');
+DEFINE ('_UDDEADM_KUNENA5', 'Kunena 5.0+');
+DEFINE ('_UDDEADM_AUTOCOMPLETER_0', 'do not load');
+DEFINE ('_UDDEADM_AUTOCOMPLETER_1', 'JS W3, case-insensitve');
+DEFINE ('_UDDEADM_AUTOCOMPLETER_2', 'JS light, case-sensitive');
+DEFINE ('_UDDEADM_AUTOCOMPLETER_3', 'Uddeim JS, look inside name');
+DEFINE ('_UDDEADM_AUTOCOMPLETER_EXP', '(<b>use autocomplete</b> must be activated)<br><i>do not load</i> no autocompleter function.<br>W3 - case-<b>insensitive</b>, <b>multiple</b> input.<br>light - case-<b>sensitive</b>');
+DEFINE ('_UDDEADM_AUTOCOMPLETER_HEAD', 'Autocompleter');
+DEFINE ('_UDDEADM_AUTOCOMPLETESTART_HEAD', 'start at letter');
+DEFINE ('_UDDEADM_AUTOCOMPLETESTART_EXP', 'number of letters to start autocompleter');
+DEFINE ('_UDDEADM_SEPERROR', 'required seperator for multiple names is ');
+DEFINE ('_UDDEADM_SEP', ' [comma]');
+DEFINE ('_UDDEADM_SEP1', ' [semicolon]');
+DEFINE ('_UDDEIM_TODP_TITLE_CC', 'One or more recipients');
+DEFINE ('_UDDEIM_TODP_TITLE_CC0', ' (comma separated)');
+DEFINE ('_UDDEIM_TODP_TITLE_CC1', ' (semicolon separated)');
+DEFINE ('_UDDEIM_DOREPLY', 'Reply');
+
+
+?>
